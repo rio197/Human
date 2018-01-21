4f1e25c9f1f3e575430bba257e8019effadedc8b
rio197 <rio197@Kedaung-PC>
2018-01-20 20:34:54 -0500

    Initial commit

---
+++ src/human/Human.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package human;
+
+/**
+ *
+ * @author rio197
+ */
+public class Human {
+
+    String name;
+    int age;
+    int heightInInches;
+    String eyeColor;
+    
+    public void speak() {
+        System.out.println("Hello, my name is " + name);
+        System.out.println("I am " + heightInInches + " inches tall");
+        System.out.println("I am " + age + " years old");
+        System.out.println("My eye color is " + eyeColor);
+    }
+}
