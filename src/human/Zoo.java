6bd5888b0b7650d33b0710061384664c1a97b194
rio197 <rio197@Kedaung-PC>
2018-01-20 22:00:54 -0500

    Add notes

--- src/human/Zoo.java
+++ src/human/Zoo.java
@@ -12,11 +12,17 @@ package human;
 public class Zoo {
     
     public static void main (String args[]) {
+        
+        int age;
+        age = 12;
+        age = 15;
+                
         Animal animal1 = new Animal(12, "M", 23);
         animal1.eat();
                 
         Bird bird1 = new Bird();
         bird1.fly();
+        
         
     }
 }
